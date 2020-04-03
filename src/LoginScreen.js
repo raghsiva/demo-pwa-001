cb659d19204810032ec092c349563f65c8117d4f
Raghul <raghsiva@outlook.com>
2020-04-03 13:20:44 +0530

    Fifth

--- src/LoginScreen.js
+++ src/LoginScreen.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import './App.css';
 import './Button.css';
-import {Redirect} from 'react-router';
-
+import {Link} from 'react-router-dom';
+import {useLocation} from 'react-router-dom'
 class LoginScreen extends React.Component{
     constructor () {
         super ();
         this.state = {
-            mofuleLinks : []
+            mofuleLinks : [],
+            username : "",
+            password : ""
         }
     }
 
+    check = () =>{
+            if(this.state.username === "verizon" && this.state.password === "verizon")
+            window.open("/myDevices", "_self")
+            else {
+                alert('Invalid Credentials')
+            }
+    }
 
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
     render() {
         return (
             <div className = "App">
@@ -25,15 +39,15 @@ class LoginScreen extends React.Component{
                             <div class = "div1">
                                 <label> Username or mobile number</label>
                             </div>
-                            <input type = "text" name = "username" style = {{width : "100%", height : "40px", "borderRadius" : "15px"}}/><br /> <br /> <br /> <br />
+                            <input type = "text" name = "username" onChange = {this.handleChange} style = {{width : "100%", height : "40px", "borderRadius" : "15px"}}/><br /> <br /> <br /> <br />
                             <div class = "div1">
                                 <label> Password</label>
                             </div>
-                            <input type = "password" name = "username" style = {{width : "100%", height : "40px", "borderRadius" : "15px"}}/><br /> <br /> <br /> <br />
+                            <input type = "password" name = "password" onChange = {this.handleChange} style = {{width : "100%", height : "40px", "borderRadius" : "15px"}}/><br /> <br /> <br /> <br />
                             
                         </form>
                     </div>    
-                    <button align = "center" onClick = {this.logincheck} class = "button">Sign In</button>
+                    <button align = "center" onClick = {this.check} class = "button">Sign In</button>
                 </div>
             </div>
         )
--- src/MyDevices.js
+++ src/MyDevices.js
@@ -11,15 +11,25 @@ class MyDevices extends React.Component{
             <div className = "App">
                 <nav class = "navbar navbar-default" style = {{height : 30, fontWeight : "bold"}}>
                     <h4 style = {{fontWeight : "bold"}}>Device Details</h4>
-                </nav>
+                </nav><hr />
             <div className = "App" style = {{margin : 30}}>
                 <h1 align = "left" style = {{fontWeight : "bold"}}>Here's your device's technical info</h1>
                 <hr style = {{height : 5, backgroundColor : "black"}}/> <br />
+            <div style = {{marginBottom : 15}}>
+                <h4 align = "left" style = {{fontWeight : "bold", margin : 0, padding :0}}> SIM ID</h4>
+                <div class = "div1"><h5 algin = "left" style = {{fontWeight : "normal", margin : 0, padding : 0}}>8914800000016996499</h5></div><br />
+                <hr />
             </div>
             <div style = {{marginBottom : 15}}>
-                <h4 align = "left" style = {{fontWeight : "bold"}}> SIM ID</h4>
-                <h5 algin = "left" style = {{fontWeight : "normal"}}>8914800000016996499</h5>
+                <h4 align = "left" style = {{fontWeight : "bold", margin : 0, padding :0}}> IMEI</h4>
+                <div class = "div1"><h5 algin = "left" style = {{fontWeight : "normal", margin : 0, padding : 0}}>352833087459695</h5></div><br />
                 <hr />
+            </div>
+            <div style = {{marginBottom : 15}}>
+                <h4 align = "left" style = {{fontWeight : "bold", margin : 0, padding :0}}> Device Model</h4>
+                <div class = "div1"><h5 algin = "left" style = {{fontWeight : "normal", margin : 0, padding : 0}}>Apple iPhone 7 256GB in silver</h5></div><br />
+                <hr />
+            </div>
             </div>
             </div>
         );
