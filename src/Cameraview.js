36f4f372d17f6ba2ce43b1d14f8175f39215dc97
Raghul <raghsiva@outlook.com>
2020-04-07 15:43:15 +0530

    Camera changes

--- src/Cameraview.js
+++ src/Cameraview.js
@@ -4,11 +4,37 @@ class Cameraview extends React.Component{
     constructor() {
         super();
     }
+    
+    showView = () => {
+        if (navigator.getUserMedia) {
+            // Request the camera.
+            navigator.getUserMedia(
+              // Constraints
+              {
+                video: true
+              },
+          
+              // Success Callback
+              function(localMediaStream) {
+          
+              },
+          
+              // Error Callback
+              function(err) {
+                // Log the error to the console.
+                console.log('The following error occurred when trying to use getUserMedia: ' + err);
+              }
+            );
+          
+          } else {
+            alert('Sorry, your browser does not support getUserMedia');
+          }
+    }
 
     render(){
         return(
             <div>
-                <Camera />
+                <input type="file" accept="image/*" capture="camera" />
             </div>
         );
     }
